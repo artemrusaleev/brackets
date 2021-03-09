f3239bdd85afc9b5444c8131160afe152bf5847a
Mariarty <artyomrusaleev@gmail.com>
2021-03-09 15:09:32 +0300

    Done

---
+++ src/index.js
@@ -0,0 +1,65 @@
+module.exports = function check(str, bracketsConfig) {
+  let stack = [];
+
+  let brackets = (() => {
+    let brackets = {};
+
+    bracketsConfig.forEach(function(pair){
+      let [opening, closing] = pair;
+
+      if (opening === closing){
+        brackets[opening] = function(){
+          let lastSymbol = stack[stack.length - 1];
+
+          if (lastSymbol === opening){
+            stack.pop();
+          } else {
+            stack.push(opening);
+          }
+
+          return true;
+        }
+      }
+
+      if (opening !== closing){
+
+        brackets[opening] = function(){
+          stack.push(opening);
+          return true;
+        }
+
+        brackets[closing] = function(){
+          let lastSymbol = stack[stack.length - 1];
+
+          if (lastSymbol === opening){
+            stack.pop();
+            return true;
+          } else {
+            return false;
+          }
+        }
+      }
+    });
+
+    return brackets;
+  })();
+
+
+
+  for (let i=0; i<str.length; i++){
+    let symbol = str[i];
+
+    if (brackets[symbol]){
+      isCorrect = brackets[symbol]();
+      if (!isCorrect){
+        return false;
+      }
+    }
+  }
+
+  if (stack.length > 0 ){
+    return false;
+  } else {
+    return true;
+  }
+}
